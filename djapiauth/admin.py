bd23bb78c0530ae9f5a298a5b9e65d4f75801752
FEI FAN <feifan.pub@gmail.com>
2015-03-08 14:27:54 -0700

    remove useless code

--- djapiauth/admin.py
+++ djapiauth/admin.py
@@ -7,21 +7,6 @@ from djapiauth.forms import get_api_key_form
 from . import API_AUTH_ADMIN_USER_FILTER
 
 
-class APIKeyForm(ModelForm):
-    class Meta:
-        model = APIKeys
-        exclude = ()
-    user = forms.ModelChoiceField(queryset=get_user_model().objects.filter(company__isnull=True),
-                                  required=True,)
-
-
-class APIKeysAdmin(admin.ModelAdmin):
-    form = APIKeyForm
-    add_form = APIKeyForm
-    list_display = ('apikey', 'user', 'note')
-    list_filter = ('apis',)
-
-
 admin.site.register(APIEntryPoint)
 admin.site.register(APIKeys,
                     form=get_api_key_form(API_AUTH_ADMIN_USER_FILTER),
