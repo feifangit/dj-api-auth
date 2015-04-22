eec99eb7b84c8568d558a3ab3efff7d043461925
FEI FAN <feifan.pub@gmail.com>
2015-04-22 15:52:53 -0700

    version update

--- djapiauth/__init__.py
+++ djapiauth/__init__.py
@@ -11,5 +11,5 @@ API_AUTH_ADMIN_USER_FILTER = _load_setting("API_AUTH_ADMIN_USER_FILTER", {})
 API_AUTH_ALLOWED_TIME_DRIFT = _load_setting("API_AUTH_ALLOWED_TIME_DRIFT", 5*60)
 
 
-__version__ = '0.5'
+__version__ = '0.6'
 VERSION = tuple(map(int, __version__.split('.')))
